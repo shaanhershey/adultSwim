0b3f0f663e7631e6f7485cb013a616000816dfda
shaanhershey <shaanhershey@gmail.com>
2021-06-09 12:45:43 -0400

    Created datapack that python script writes to

--- script/process.py
+++ script/process.py
@@ -4,10 +4,29 @@ im = Image.open("../logo.jpeg")
 pix = im.load()
 width, length = im.size
 
+center_x = 100
+center_y = 100
+
+coords = []
+
 for x in range(width):
     for y in range(length):
-        if(pix[x,y][0] <= 150):
+        if(pix[x,y][0] <= 200):
             pix[x,y] = (0,0,0)
         else :
             pix[x,y] = (255,255,255)
+            coords.append(((x-center_x)/27, (y-center_y)/27))
+
 im.save('../processed.jpeg', 'JPEG')
+
+# /execute as @a at @s run summon minecraft:tnt ~ ~-90 ~ {Fuse:40,Motion[x,1.0,y]}
+print(len(coords))
+# f.write("execute as @a at @s run summon minecraft:tnt ~ ~-90 ~ {Fuse:40, Motion:[%s,1.0,%s]}" % coords[0])
+for i in range(8):
+    f = open("../tntPicture/data/picture/functions/picture%s.mcfunction" % (i), "w")
+    for j in range(56):
+        if(j == 55 and (i == 7 or i == 6)):
+            break
+        if(j*128+i*16 >= 7136):
+            print("%s,%s" % (j,i))
+        f.write("execute as @a at @s run summon minecraft:tnt ~ ~-90 ~ {Fuse:40, Motion:[%s,1.0,%s]}\n" % coords[j*128+i*16])
---
+++ tntPicture/data/picture/functions/load.mcfunction
@@ -0,0 +1,1 @@
+tellraw @a {"text": "Loaded", "color": "blue"}
